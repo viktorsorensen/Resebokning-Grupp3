1a87685bb41d80c0bcebfc4d7bfb3dafa9164626
viktorsorensen <viktor.sorensen87@outlook.com>
2016-04-06 19:48:26 +0200

    Fixed Datepickers

--- src/controller/BookingViewController.java
+++ src/controller/BookingViewController.java
@@ -10,12 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import java.awt.*;
-import java.awt.TextField;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.util.Callback;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 
 /**
  * Created by viktorsorensen on 2016-04-03.
@@ -68,20 +70,33 @@ public class BookingViewController implements Initializable {
     @FXML
     private RadioButton rbtnOneWay;
 
-
-
-
-
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        // Lägg in dummy data
         cbFrom.setItems(FXCollections.observableArrayList("Ängelholm", "Malmö", "Helsingborg"));
         cbTo.setItems(FXCollections.observableArrayList("Mexiko", "Danmark", "Finland"));
         cbChooseClass.setItems(FXCollections.observableArrayList("Välj klass", new Separator(), "Första Klass" , "Business", "Ekonomi"));
         cbChooseClass.getSelectionModel().selectFirst();
-
+        
+        // Datepicker
+        dateDepart.setValue(LocalDate.now());
+        final Callback<DatePicker, DateCell> dayCellFactory =
+                (final DatePicker datePicker) -> new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        
+                        if (item.isBefore(
+                                dateDepart.getValue().plusDays(1))
+                                ) {
+                            setDisable(true);
+                            setStyle("-fx-background-color: #ffc0cb");
+                        }
+                    }
+                };
+        
+        dateReturn.setDayCellFactory(dayCellFactory);
+        dateReturn.setValue(dateDepart.getValue().plusDays(1));
     }
 
     @FXML
