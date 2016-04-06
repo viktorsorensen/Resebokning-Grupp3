30d4a6fe97551652bb04f9c545544f91fcf796ef
viktorsorensen <viktor.sorensen87@outlook.com>
2016-04-06 23:33:26 +0200

    Changed datepicker

--- src/controller/BookingViewController.java
+++ src/controller/BookingViewController.java
@@ -9,9 +9,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Locale;
@@ -76,7 +80,7 @@ public class BookingViewController implements Initializable {
         // Lägg in dummy data
         cbFrom.setItems(FXCollections.observableArrayList("Ängelholm", "Malmö", "Helsingborg"));
         cbTo.setItems(FXCollections.observableArrayList("Mexiko", "Danmark", "Finland"));
-        cbChooseClass.setItems(FXCollections.observableArrayList("Välj klass", new Separator(), "Första Klass" , "Business", "Ekonomi"));
+        cbChooseClass.setItems(FXCollections.observableArrayList("Välj klass", new Separator(), "Första Klass", "Business", "Ekonomi"));
         cbChooseClass.getSelectionModel().selectFirst();
         
         // Datepicker
@@ -90,15 +94,29 @@ public class BookingViewController implements Initializable {
                         long daysGone = ChronoUnit.DAYS.between(dateDepart.getValue(), item);
                         setTooltip(new Tooltip("Du kommer att vara borta i " + daysGone + " dagar."));
                         
-                        if (item.isBefore(
-                                dateDepart.getValue().plusDays(1))
-                                ) {
+                        if (item.isBefore(dateDepart.getValue().plusDays(1))) {
                             setDisable(true);
                             setStyle("-fx-background-color: #ffc0cb");
                         }
+
                     }
                 };
-        
+
+        final Callback<DatePicker, DateCell> dayCellFactoryDepart =
+                (final DatePicker datePicker) -> new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        DayOfWeek day = DayOfWeek.from(item);
+                        if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
+                            setStyle("-fx-background-color: beige");
+                        }
+
+                    }
+                };
+
+        dateDepart.setDayCellFactory(dayCellFactoryDepart);
         dateReturn.setDayCellFactory(dayCellFactory);
         dateReturn.setValue(dateDepart.getValue().plusDays(1));
     }
