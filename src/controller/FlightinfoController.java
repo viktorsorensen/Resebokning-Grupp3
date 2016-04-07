ea17d3cea182b45372feb503ef15b4a50abec797
viktorsorensen <viktor.sorensen87@outlook.com>
2016-04-07 23:04:19 +0200

    Added backbutton to FlightInfoScene

--- src/controller/FlightinfoController.java
+++ src/controller/FlightinfoController.java
@@ -1,17 +1,43 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by Namir on 4/7/2016.
  */
+
 public class FlightinfoController implements Initializable{
+
+    @FXML
+    private Button backBtn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    private void backAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+
+        stage = (Stage) backBtn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("../view/BookingView.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
