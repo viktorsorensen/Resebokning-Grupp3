7b41fe784699eb3a1b1aee036a4f146a35617f17
BATMUS1 <bohlin.hampus@gmail.com>
2016-04-07 16:27:17 +0200

    Fixat med Knappar

--- src/controller/BookingViewController.java
+++ src/controller/BookingViewController.java
@@ -9,20 +9,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.util.Callback;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+
+import javax.swing.*;
 
 /**
  * Created by viktorsorensen on 2016-04-03.
@@ -53,9 +50,11 @@ public class BookingViewController implements Initializable {
 
     @FXML
     private TextField tfAdult;
+    int adultCount = 0;
 
     @FXML
     private TextField tfChild;
+    int childCount = 0;
 
     @FXML
     private Button btnAdultMinus;
@@ -75,12 +74,13 @@ public class BookingViewController implements Initializable {
     @FXML
     private RadioButton rbtnOneWay;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Lägg in dummy data
         cbFrom.setItems(FXCollections.observableArrayList("Ängelholm", "Malmö", "Helsingborg"));
         cbTo.setItems(FXCollections.observableArrayList("Mexiko", "Danmark", "Finland"));
-        cbChooseClass.setItems(FXCollections.observableArrayList("Välj klass", new Separator(), "Första Klass", "Business", "Ekonomi"));
+        cbChooseClass.setItems(FXCollections.observableArrayList("Välj klass", new Separator(), "Första Klass" , "Business", "Ekonomi"));
         cbChooseClass.getSelectionModel().selectFirst();
         
         // Datepicker
@@ -90,39 +90,61 @@ public class BookingViewController implements Initializable {
                     @Override
                     public void updateItem(LocalDate item, boolean empty) {
                         super.updateItem(item, empty);
-
-                        long daysGone = ChronoUnit.DAYS.between(dateDepart.getValue(), item);
-                        setTooltip(new Tooltip("Du kommer att vara borta i " + daysGone + " dagar."));
                         
-                        if (item.isBefore(dateDepart.getValue().plusDays(1))) {
+                        if (item.isBefore(
+                                dateDepart.getValue().plusDays(1))
+                                ) {
                             setDisable(true);
                             setStyle("-fx-background-color: #ffc0cb");
                         }
                     }
                 };
-
-        final Callback<DatePicker, DateCell> dayCellFactoryDepart =
-                (final DatePicker datePicker) -> new DateCell() {
-                    @Override
-                    public void updateItem(LocalDate item, boolean empty) {
-                        super.updateItem(item, empty);
-
-                        DayOfWeek day = DayOfWeek.from(item);
-                        if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
-                            setStyle("-fx-background-color: beige");
-                        }
-                    }
-                };
-
-        dateDepart.setDayCellFactory(dayCellFactoryDepart);
+        
         dateReturn.setDayCellFactory(dayCellFactory);
         dateReturn.setValue(dateDepart.getValue().plusDays(1));
+
+
     }
 
     @FXML
-    private void handleAction(ActionEvent event) throws  IOException{
+    private void addAdult(ActionEvent ev) throws IOException {
+
+        adultCount++;
+        tfAdult.setText(String.valueOf(adultCount));
+        if (adultCount >= 9){
+            tfAdult.setText(String.valueOf(8));
+        }
+
+        }
+
+    @FXML
+    private void removeAdult(ActionEvent ev) throws IOException {
+        adultCount--;
+        tfAdult.setText(String.valueOf(adultCount));
+        if(adultCount < 0){
+            tfAdult.setText(String.valueOf(0));
+        }
 
     }
+    @FXML
+    private void addChild(ActionEvent ev) throws IOException {
+        childCount++;
+        tfChild.setText(String.valueOf(childCount));
+        if(childCount >= 8){
+            tfChild.setText(String.valueOf(8));
+        }
+    }
+    @FXML
+    private void removeChild(ActionEvent ev) throws IOException {
+        childCount--;
+        tfChild.setText(String.valueOf(childCount));
+        if(childCount < 0){
+            tfChild.setText(String.valueOf(0));
+        }
+    }
+
+
+
     @FXML
     private void handleAction(ActionEvent event) throws IOException {
         Stage stage;
