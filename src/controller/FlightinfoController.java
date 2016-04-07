8afcb9bfe0e21a7552983696096cebe6c2c71fae
Namirwesaf <Namir_wesaf@hotmail.com>
2016-04-07 22:33:43 +0200

    Made 3th scene

---
+++ src/controller/FlightinfoController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Namir on 4/7/2016.
+ */
+public class FlightinfoController implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
+
